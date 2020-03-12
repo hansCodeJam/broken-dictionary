f216649e1ce5ebdbfda4de3e3995cd78a58eeb4f
Hans Vos <hans.vos@codeimmersives.com>
2020-03-12 15:45:03 -0400

    fixed code

--- app.js
+++ app.js
@@ -24,10 +24,12 @@ mongoose.connect(process.env.MONGODB_URI,{
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 
+app.use(express.json());
+app.use(express.urlencoded({extended: false }))
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
